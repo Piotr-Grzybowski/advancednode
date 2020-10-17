cad693e4a087e002ac57b08ab777c7972c60ac06
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2020-10-17 22:03:09 +0200

    Connect DB

--- server.js
+++ server.js
@@ -17,26 +17,35 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 }));
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  });
-});
 app.use(passport.initialize());
 app.use(passport.session());
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.render(process.cwd() + '/views/pug', {title: 'Hello', message: 'Please login'});
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  app.route("/").get((req, res) => {
+    //Change the response to render the Pug template
+    res.render(process.cwd() + '/views/pug', { title: 'Connected to Database', message: 'Please login' });
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render(process.cwd() + '/views/pug', { title: e, message: 'Unable to login' })
+  })
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+  console.log("Listening on port " + process.env.PORT || 3000);
 });
