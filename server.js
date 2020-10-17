be941995af59f576cf5f2aac19ed8291a2894a08
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2020-10-17 22:21:23 +0200

    Add local auth strategy

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require('express-session');
 const passport = require('passport');
 const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local');
 
 const app = express();
 app.set('view engine', 'pug');
@@ -25,6 +26,18 @@ app.use(express.urlencoded({ extended: true }));
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
+
+  passport.use(new LocalStrategy(
+    (username, password, done) => {
+      myDataBase.findOne({ username: username }, (err, user) => {
+        console.log('User ' + username +' attempted to log in.');
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        if (password !== user.password) return done(null, false);
+        return done(null, user);
+      })
+    }
+  ))
 
   app.route("/").get((req, res) => {
     //Change the response to render the Pug template
