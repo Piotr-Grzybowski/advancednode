99335005e362a547d537fd516ca321c19035bd4e
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2020-10-17 16:59:43 +0200

    Add pug

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.render('Load your view here');
+  res.render('/');
 });
 
 app.listen(process.env.PORT || 3000, () => {
