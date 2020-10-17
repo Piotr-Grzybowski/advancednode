387a346a8baf84f9c7ef206493838be82e6bcbdf
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2020-10-17 21:46:14 +0200

    Serialize and deserialize user

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require('express-session');
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 app.set('view engine', 'pug');
@@ -16,6 +17,15 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: false }
 }));
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDB.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+    done(null, null);
+  });
+});
 app.use(passport.initialize());
 app.use(passport.session());
 app.use("/public", express.static(process.cwd() + "/public"));
