64fc6fef08e40fe84cebfe747320bed1ac3a7fc2
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2020-10-17 18:09:02 +0200

    Start Pug

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const myDB = require('./connection');
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
+app.set('view engine', 'pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'pug');
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.render('./views/pug/index');
+  res.render(process.cwd() + '/views/pug');
 });
 
 app.listen(process.env.PORT || 3000, () => {
