fd71355def38e53ed34706e4c6d23bde4fc18b64
Piotr Grzybowski <pgrzybowski.dev@gmail.com>
2020-10-17 22:48:32 +0200

    Show profile to user

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ myDB(async client => {
   })
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
-    res.render(process.cwd() + '/views/pug/profile', { username: req.body.username });
+    res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
   })
 
   passport.serializeUser((user, done) => {
